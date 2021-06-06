65eecadac2b614d261201b870ba4c42dc8f7cf93
Gunjan Agarwal <68350045+gunjan2405@users.noreply.github.com>
2021-06-06 21:05:21 +0530

    Add files via upload

---
+++ src/components/Note.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Note() {
+    return (
+        <>
+            <div className = "note">
+                <h1>Javascript and React JS</h1>
+                <p>This was an amazing bootcamp taken up by Shaurya Sinha. We covered everything from scratch including HTML, Javascript, React JS.</p>
+            </div>
+            <div className = "note">
+                <h1>Python and Machine learning</h1>
+                <p>This was an amazing bootcamp taken up by Shaurya Sinha. We covered everything from scratch including Python,  Machine Learning fundamentals.</p>
+            </div>
+            <div className = "note">
+                <h1>Python and Deep learning</h1>
+                <p>This was an amazing bootcamp taken up by Shaurya Sinha. We covered everything from scratch including Python,   Deep Learning fundamentals.</p>
+            </div>
+            <div className = "note">
+                <h1> Basic Web Dev with Bootstrap</h1>
+                <p>This was an amazing bootcamp taken up by Shaurya Sinha. We covered everything from scratch including CSS, Bootstrap CSS, etc.</p>
+            </div>
+            <div className = "note">
+                <h1>Python and Data Analytics</h1>
+                <p>This was an amazing bootcamp taken up by Shaurya Sinha. We covered everything from scratch including Python and Data Analytics</p>
+            </div>
+            <div className = "note">
+                <h1>Python and Computer Vision</h1>
+                <p>This was an amazing bootcamp taken up by Shaurya Sinha. We covered everything from scratch including Python and Computer Vision</p>
+            </div>
+        </>
+    )
+}
+
+export default Note
